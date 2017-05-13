da313a4a723756639015ffbacb36ac759b35c6fe
rodrigih <rodrigih@uci.edu>
2017-05-12 19:23:15 -0700

    Now plays sequence. Allows player input.

--- src/app/gameboard.component.ts
+++ src/app/gameboard.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
-
 import { trigger,
          state,
          style,
          animate,
          transition } from '@angular/animations';
+import { GameLogicService } from './game-logic.service';
 
 @Component({
     selector: 'game-board',
     templateUrl: './gameboard.component.html',
     styleUrls: ['./gameboard.component.css'],
+    providers: [ GameLogicService ],
     animations: [
         trigger('changeColour',[
 
@@ -54,8 +55,12 @@ import { trigger,
 })
 
 export class GameBoardComponent {
+  constructor(private gameLogicService: GameLogicService) { }
 
-  // Object holds colours to use when piece is lit
+  // Flag used to determine whether gameboard is busy playing a sequence
+  busy = false;
+
+  // Object holds states of each individual colour
   colourStates = {
     'green': 'green',
     'red': 'red',
@@ -63,8 +68,58 @@ export class GameBoardComponent {
     'blue': 'blue'
   }
 
+  // Holds players sequence
+  index = 0;
+
   lightPiece(colour: string){
     var state = this.colourStates[colour];
     this.colourStates[colour] = (state === colour?colour+'Lit':colour)
   }
+
+  pressPiece(colour: string){
+    if(this.busy){return;}
+
+    var status = this.gameLogicService.checkSequence(colour);
+    console.log(status);
+
+    this.lightPiece(colour);
+    setTimeout(this.lightPiece.bind(this),500,colour);
+
+    if(status === 'wrong'){
+      //TODO: notify player is wrong
+      console.log('wrong');
+      this.playSequence();
+    }
+
+    else if (status === 'new'){
+      this.playSequence();
+    }
+    else if (status === 'gameOver'){
+      console.log('Game Over');
+    }
+  }
+
+  playSequence(){
+     var seq = this.gameLogicService.getSequence();
+     var index = 0;
+
+     this.busy = true;
+     console.log(index);
+
+     var id = setInterval(() => {
+      console.log(index);
+      if(index >= seq.length) {
+        clearInterval(id);
+        console.log('cleared');
+        this.busy = false;
+        return;
+      }
+
+      var c = seq[index];
+      index++;
+      this.lightPiece(c);
+      setTimeout(this.lightPiece.bind(this),1000,c);
+    },1500);
+  }
+
 }
