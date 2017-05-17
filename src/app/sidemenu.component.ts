b759f47dd72d8dd8bf39bcb263d903346bd3b866
rodrigih <rodrigih@uci.edu>
2017-05-17 16:20:02 -0700

    Changed gameboard style. Edited side menu

--- src/app/sidemenu.component.ts
+++ src/app/sidemenu.component.ts
@@ -3,11 +3,11 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'side-menu',
     templateUrl: './sidemenu.component.html',
-    styleUrls: ['./sidemenu.component.css']
+    styleUrls: ['./sidemenu.component.css'],
 })
 
 export class SideMenuComponent{
-
+  state = 'hidden';
   items = ['How To Play', 'Mode', 'High Score', 'Credits'];
-  
+
 }
